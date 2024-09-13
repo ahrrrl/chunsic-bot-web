7c92173987907216082bd7ce95738cee354e566d
mokoko <guswlso2@naver.com>
2024-09-13 19:00:53 +0900

    design: docs 페이지 반응형 레이아웃 수정

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './docs.module.scss';
 import { toc } from './toc';
 import RightSidebar from '../components/rightSidebar';
@@ -18,6 +18,7 @@ export interface Heading {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [headings, setHeadings] = useState<Heading[]>([]);
+  const [isHeaderOpen, setIsHeaderOpen] = useState(false);
 
   const handleClick = (id: string) => {
     const element = document.getElementById(id);
@@ -26,25 +27,55 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     }
   };
 
+  const handleResize = () => {
+    if (window.innerWidth > 700) {
+      setIsHeaderOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles.docs}>
-      <aside className={`${styles.sidebar} ${styles.left}`}>
-        <Link href='/docs'>
-          <div className={styles.docTitleBox}>
-            <span className={styles.docTitle}>춘식이 1.0 설명서</span>
-          </div>
-        </Link>
-        <LeftSidebar toc={toc} />
-      </aside>
-      <div className={styles.mainContent}>
-        <MainContent setHeadings={setHeadings}>
-          {children}
-          <ChildrenCard toc={toc} />
-        </MainContent>
+      <div
+        className={styles.docsHeader}
+        onClick={() => setIsHeaderOpen(!isHeaderOpen)}
+      >
+        차례
       </div>
-      <aside className={`${styles.sidebar} ${styles.right}`}>
-        <RightSidebar headings={headings} handleClick={handleClick} />
-      </aside>
+      {isHeaderOpen ? (
+        <div
+          className={styles.headerList}
+          onClick={() => setIsHeaderOpen(!isHeaderOpen)}
+        >
+          <LeftSidebar toc={toc} />
+        </div>
+      ) : (
+        <>
+          <aside className={`${styles.sidebar} ${styles.left}`}>
+            <Link href='/docs'>
+              <div className={styles.docTitleBox}>
+                <span className={styles.docTitle}>춘식이 1.0 설명서</span>
+              </div>
+            </Link>
+            <LeftSidebar toc={toc} />
+          </aside>
+          <div className={styles.mainContent}>
+            <MainContent setHeadings={setHeadings}>
+              {children}
+              <ChildrenCard toc={toc} />
+            </MainContent>
+          </div>
+          <aside className={`${styles.sidebar} ${styles.right}`}>
+            <RightSidebar headings={headings} handleClick={handleClick} />
+          </aside>
+        </>
+      )}
     </div>
   );
 }
